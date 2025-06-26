4ccf440dc93f8416749a7b00c8093b18c6c4a964
Yonathan-T <itsyonathantaweke@gmail.com>
2025-06-26 20:01:41 +0300

    fix: return view

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    view('welcome');
+    return view('welcome');
 });
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
